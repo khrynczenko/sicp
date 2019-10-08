8fcfb9386615af74b5e56393d070e3d086eed1ed
Krzysztof Hrynczenko <jeniopy@gmail.com>
2019-10-08 23:43:23 +0200

    Added exercises for 1.2.3

---
+++ chapter-1/section-3/exercises.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+
+
+;; Exercise 1.14
+;; For space its O(n)
+;; For step its O(n^k)
+
+;; Exercise 1.15
+;; a) Procedure p is applied 5 times
+;; b) Order of growth and space are the same. It is equal to number of invocations of p and that is every power of 3 within range of argument.
